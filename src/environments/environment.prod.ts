0ce14b292d2736521b6d7c1b11baeb48f4f0519b
bauyrzhan <bauyrzhan@genesiszero.com>
2018-03-27 18:25:54 +0600

    env details changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  HyperledgerApi: 'http://184.72.113.124:3000/api/',
-  BackendApi: 'http://184.72.113.124:3005/api/v1/',
-  socket_io: 'http://184.72.113.124:3005'
+  HyperledgerApi: 'http://34.230.41.83:3000/api/',
+  BackendApi: 'http://34.230.41.83:3005/api/v1/',
+  socket_io: 'http://34.230.41.83:3005'
 };
